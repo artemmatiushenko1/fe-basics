6bd73ab6796f48f2ecf186cd09b9e37c386510bb
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-10-07 22:57:16 +0300

    Added tasks implementation for lab 4

---
+++ lab-4/script.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/*-------TASK-1-------*/
+
+const refs = {
+  firstElement: document.getElementById('first-element'),
+  secondElement: document.querySelector('.second-element'),
+};
+
+refs.firstElement.addEventListener('click', () => {
+  refs.firstElement.classList.toggle('first-element--active');
+});
+
+refs.secondElement.addEventListener('click', (e) => {
+  e.stopPropagation();
+  refs.secondElement.classList.toggle('second-element--active');
+});
+
+/*-------TASK-1-------*/
+
+/*-------TASK-2-------*/
+
+class ImgViewer {
+  refs = {
+    addImgButton: document.querySelector('.add-img-button'),
+    removeImgButton: document.querySelector('.remove-img-button'),
+    zoomInImgButton: document.querySelector('.zoom-in-img-button'),
+    zoomOutImgButton: document.querySelector('.zoom-out-img-button'),
+    resetZoomButton: document.querySelector('.reset-zoom-button'),
+    imgContainer: document.querySelector('.img-container'),
+  };
+
+  currentZoom = 1;
+  zoomStep = 0.2;
+
+  constructor() {
+    this.refs.addImgButton.addEventListener('click', this.add);
+    this.refs.removeImgButton.addEventListener('click', this.remove);
+    this.refs.zoomInImgButton.addEventListener('click', this.zoomIn);
+    this.refs.zoomOutImgButton.addEventListener('click', this.zoomOut);
+    this.refs.resetZoomButton.addEventListener('click', this.resetZoom);
+  }
+
+  getImg = () => {
+    return this.refs.imgContainer.querySelector('img');
+  };
+
+  add = () => {
+    let img = this.getImg();
+    if (img) return;
+
+    img = document.createElement('img');
+    img.src = './assets/sydney-image.webp';
+    img.style.transition = 'transform 0.5s ease';
+    img.style.width = '400px';
+
+    this.refs.imgContainer.appendChild(img);
+  };
+
+  remove = () => {
+    const img = this.getImg();
+    if (!img) return;
+
+    this.refs.imgContainer.removeChild(img);
+    this.currentZoom = 1;
+  };
+
+  zoomIn = () => {
+    const img = this.getImg();
+    if (!img) return;
+
+    const nextZoomValue = this.currentZoom + this.zoomStep;
+    img.style.transform = `scale(${nextZoomValue})`;
+    this.currentZoom += this.zoomStep;
+  };
+
+  zoomOut = () => {
+    const img = this.getImg();
+    if (!img) return;
+
+    const nextZoomValue = this.currentZoom - this.zoomStep;
+    if (nextZoomValue <= this.zoomStep) return;
+
+    img.style.transform = `scale(${nextZoomValue})`;
+    this.currentZoom -= this.zoomStep;
+  };
+
+  resetZoom = () => {
+    const img = this.getImg();
+    if (!img) return;
+
+    this.currentZoom = 1;
+    img.style.transform = `scale(${this.currentZoom})`;
+  };
+}
+
+const imgViewer = new ImgViewer();
+
+/*-------TASK-2-------*/
